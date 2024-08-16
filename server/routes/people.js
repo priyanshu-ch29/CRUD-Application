9ed455439579cc5d6178f2e19e6baf9c3f528f5c
priyanshu-ch29 <priyanshuchaudhary1000@gmail.com>
2024-08-17 00:59:15 +0530

    done

---
+++ server/routes/people.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllPeople,
+  addPerson,
+  getPerson,
+  updatePerson,
+  deleteJob,
+} = require("../controllers/people");
+
+router.route("/people").get(getAllPeople).post(addPerson);
+router
+  .route("/people/:id")
+  .get(getPerson)
+  .patch(updatePerson)
+  .delete(deleteJob);
+
+module.exports = router;
